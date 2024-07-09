cc7e614d1955ad70e23aa06dd08e67172a1d7d34
Tjark <tjarksievers@icloud.com>
2024-07-09 13:55:31 +0200

    New jobs with saving superfluid weight

--- code/minimise/profile_results.py
+++ code/minimise/profile_results.py
@@ -1,5 +1,5 @@
 import pstats
 
-p = pstats.Stats('test_results/profile')
+p = pstats.Stats('profiling/profile_0_0_3')
 p.sort_stats('tottime').print_stats(30)
 #p.sort_stats('ncalls').print_stats(100)
